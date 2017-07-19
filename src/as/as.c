c499bcc4b3218eac798fa0df984eda01ffefb9a6
Brandon Nguyen <brandontrinhnguyen@gmail.com>
2017-07-19 00:05:33 -0700

    Added test tokenizer to as

--- src/as/as.c
+++ src/as/as.c
@@ -1,8 +1,17 @@
 #include <stdio.h>
 
+#include <brandon/tok.h>
+
 int main( int argc, char * argv[] )
 {
-    printf("Hello world!\n");
+    // test tokenizer
+    const char * str = "hello world this is a tokenized string";
+
+    Tokenizer * tok = Tokenizer_new( str, " " );
+    while( Tokenizer_hasTokens( tok ) )
+    {
+        printf("%s\n", Tokenizer_next( tok ) );
+    }
 
     return 0;
 }
