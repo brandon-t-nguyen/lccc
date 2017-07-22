6875f48e9dac6cb516cf42752cb5358f09306264
Brandon Nguyen <brandontrinhnguyen@gmail.com>
2017-07-22 13:54:40 -0700

    as: refactored; going to use FSM design

--- src/as/as.c
+++ src/as/as.c
@@ -4,29 +4,114 @@
 
 #include <brandon/tok.h>
 #include <brandon/vvector.h>
+#include <brandon/cstr.h>
 
 #include "as.h"
 
+
+#define ARRAY_LEN(x) (sizeof(x)/sizeof(x[0]))
+
+const AsmOpProp pattOps[] =
+{
+    {"add",AsmOp_add,AsmOpClass_Concrete},
+    {"and",AsmOp_and,AsmOpClass_Concrete},
+    {"not",AsmOp_not,AsmOpClass_Concrete},
+    {"br",AsmOp_br,AsmOpClass_Concrete},{"brn",AsmOp_br,AsmOpClass_Concrete},
+    {"brnz",AsmOp_br,AsmOpClass_Concrete},{"brnp",AsmOp_br,AsmOpClass_Concrete},
+    {"brnzp",AsmOp_br,AsmOpClass_Concrete},{"brz",AsmOp_br,AsmOpClass_Concrete},
+    {"brzp",AsmOp_br,AsmOpClass_Concrete},{"brp",AsmOp_br,AsmOpClass_Concrete},
+    {"nop",AsmOp_nop,AsmOpClass_Concrete},
+    {"jmp",AsmOp_jmp,AsmOpClass_Concrete},
+    {"jsr",AsmOp_jsr,AsmOpClass_Concrete},
+    {"jsrr",AsmOp_jsrr,AsmOpClass_Concrete},
+    {"ret",AsmOp_ret,AsmOpClass_Concrete},
+    {"ld",AsmOp_ld,AsmOpClass_Concrete},
+    {"ldr",AsmOp_ldr,AsmOpClass_Concrete},
+    {"ldi",AsmOp_ldi,AsmOpClass_Concrete},
+    {"st",AsmOp_st,AsmOpClass_Concrete},
+    {"str",AsmOp_str,AsmOpClass_Concrete},
+    {"sti",AsmOp_sti,AsmOpClass_Concrete},
+
+    {".orig",AsmOp_orig,AsmOpClass_Meta},
+    {".blkw",AsmOp_blkw,AsmOpClass_Concrete},
+    {".fill",AsmOp_fill,AsmOpClass_Concrete},
+    {".stringz",AsmOp_stringz,AsmOpClass_Concrete}
+};
+
+// backward compatible
+const AsmOpProp lcccOps[] =
+{
+    {"mov",AsmOp_mov,AsmOpClass_Concrete},
+    {"or",AsmOp_or,AsmOpClass_Concrete},
+    {"neg",AsmOp_neg,AsmOpClass_Concrete},
+    {"push",AsmOp_push,AsmOpClass_Concrete},
+    {"pop",AsmOp_pop,AsmOpClass_Concrete},
+
+    {".string",AsmOp_stringz,AsmOpClass_Concrete},
+    {".stringp",AsmOp_stringp,AsmOpClass_Concrete},
+    {".define",AsmOp_define,AsmOpClass_Meta},
+    {".section",AsmOp_section,AsmOpClass_Meta},
+    {".export",AsmOp_export,AsmOpClass_Meta},
+    {".import",AsmOp_import,AsmOpClass_Meta}
+};
+
+AsmOp parseOpcode( const char * str, AsmSettings * settings )
+{
+    for (int i = 0; i < ARRAY_LEN(pattOps); ++i)
+    {
+        if (!strcmp_caseless( pattOps[i].match, str ))
+        {
+            return pattOps[i].op;
+        }
+    }
+
+    if (settings->syntax == AsmSyntax_patt)
+    {
+        return AsmOp_invalid;
+    }
+
+    for (int i = 0; i < ARRAY_LEN(lcccOps); ++i)
+    {
+        if (!strcmp_caseless( lcccOps[i].match, str ))
+        {
+            return lcccOps[i].op;
+        }
+    }
+
+    return AsmOp_invalid;
+}
+
+void processSource( Source * src, AsmSettings * set )
+{
+    while (Tokenizer_hasTokens( src->tok ))
+    {
+        const char * token = Tokenizer_next( src->tok );
+        if (!strcmp(token,"\n"))
+        {
+            token = "\\n";
+        }
+        printf("Token: %s\n",token);
+    }
+}
+
 char testProg[] = "; Brandon Nguyen\n"
-                  "; Go fuck yourself\n"
+                  "; lorem ipsum\n"
                   "    .section .text\n"
                   "\n"
+                  "hello .stringz \"Hello world!\\n\";\n"
                   "main\n"
                   "    add r0, r0, r1;bullshit lmao\n"
                   ;
 
-extern VVector * processSource( const char * src );
 int main( int argc, char * argv[] )
 {
-    VVector * lines = processSource( testProg );
+    // Driver: get a string containing the entire source
+    Source src;
+    Source_ctor( &src, testProg, "testProg" );
 
-    int len = VVector_length( lines );
-    for (int i = 0; i < len; ++i)
-    {
-        Line * line = (Line *)VVector_get( lines, i );
-        printf( "%4d %s\n", line->number, line->code );
-    }
+    AsmSettings set = {.syntax = AsmSyntax_patt};
 
-    VVector_delete( lines );
+    processSource( &src, &set );
+
     return 0;
 }
--- src/as/as.h
+++ src/as/as.h
@@ -7,33 +7,112 @@ extern "C" {
 
 #include <stdint.h>
 
+#include <brandon/vvector.h>
+#include <brandon/tok.h>
+
 typedef enum AsmSyntax_enum
 {
     AsmSyntax_patt,
     AsmSyntax_lccc
 } AsmSyntax;
 
+typedef struct AsmSettings_str
+{
+    AsmSyntax syntax;
+} AsmSettings;
+
+
+
+typedef struct Source_str
+{
+    char * name;    // where this came from
+    char * raw;     // raw code
+
+    int numLines;   // number of lines
+    char ** lines;  // array of lines
+
+    Tokenizer * tok;    // tokenizer for the source
+} Source;
+void Source_ctor( Source * thiz, const char * src, const char * name );
+void Source_dtor( Source * thiz );
+
 typedef struct Program_str
 {
-    VVector * localSymbols;     // symbols declared in this unit
-    VVector * externalSymbols;  // symbols that are
-    VVector * exportSymbols;
+    VVector(Symbol) * localSymbols;     // symbols declared in this unit
+    VVector(Symbol) * externalSymbols;  // symbols that are
+    VVector(Symbol) * exportSymbols;
+    VVector(Symbol) * code;
 } Program;
 
 typedef struct Symbol_str
 {
+    int external;
+    int32_t offset;
 } Symbol;
-
-typedef struct Line_str
-{
-    int number;
-    const char * code;    // filtered code
-} Line;
 
 typedef enum ParseError_enum
 {
     ParseError_Okay,
+    ParseError_InvalidOpcode,
+    ParseError_SymbolMultiplyDeclare,
+    ParseError_OtherError
 } ParseError;
+
+typedef enum AsmOp_enum
+{
+    // original operations
+    AsmOp_add,
+    AsmOp_and,
+    AsmOp_not,
+    AsmOp_br,
+    AsmOp_nop,
+    AsmOp_jmp,
+    AsmOp_jsr,
+    AsmOp_jsrr,
+    AsmOp_ret,
+    AsmOp_ld,
+    AsmOp_ldr,
+    AsmOp_ldi,
+    AsmOp_st,
+    AsmOp_str,
+    AsmOp_sti,
+    AsmOp_trap,
+    AsmOp_rti,
+    // original pseudo-ops
+    AsmOp_orig,
+    AsmOp_blkw,
+    AsmOp_fill,
+    AsmOp_stringz,
+
+    // new operations
+    AsmOp_mov,
+    AsmOp_or,
+    AsmOp_neg,
+    AsmOp_push,
+    AsmOp_pop,
+    // new pseudo-ops
+    AsmOp_stringp,
+    AsmOp_define,
+    AsmOp_section,
+    AsmOp_export,
+    AsmOp_import,
+
+    //
+    AsmOp_invalid
+} AsmOp;
+
+typedef enum AsmOpClass_enum
+{
+    AsmOpClass_Concrete,    // linkable; creates data
+    AsmOpClass_Meta,        // doesn't create data
+} AsmOpClass;
+
+typedef struct AsmOpProp_str
+{
+    const char * match; // pattern to match
+    AsmOp op;           // resulting op
+    AsmOpClass cls;     // class of operation
+} AsmOpProp;
 
 #ifdef __cplusplus
 }
